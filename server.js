49e9e0bb10fa6281886d7743f96684866f02adf6
Fazliddin1092 <fazliddin.vakhobov.ai@gmail.com>
2022-10-10 19:55:25 +0900

    adding login feat

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
+//packages
 const express = require("express");
 const path = require("path")
 const app = express();
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
+const allRoutes = require("./router");
+const session = require("express-session");
 
+//view engine
 app.set("view engine", "ejs")
-app.use('/static', express.static(path.join(__dirname, 'public')))
 
-app.get("/", (req, res) => {
-    res.render('base', {title: "Login"});
-})
+//public folders
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public/assets')));
 
+//session
+app.use(session({
+  secret: uuidv4(),
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+//getting data as json 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//endpoints
+app.use("/", allRoutes);
+
+
+//server 
 const port = process.env.PORT || 8000;
 
 app.listen(port, (err)=>{
